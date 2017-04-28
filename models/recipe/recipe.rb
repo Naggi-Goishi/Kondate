bea3d0fc21f930d16381494943cbbf1f86fe8cec
Naggi-Goishi <naggitapdance@gmail.com>
2017-04-28 14:41:22 -0700

    debug

--- models/recipe/recipe.rb
+++ models/recipe/recipe.rb
@@ -4,16 +4,11 @@ require_relative './scraping'
 class Recipe < ActiveRecord::Base
   include Scraping
 
-  belongs_to :recipe_kind
+  scope :main,   -> { includes(:recipe_kind).where.not(recipe_kinds: {name: "デザート"}) }
+  scope :random, -> { order('RAND()').first }
+  scope :where_recipe_kind, -> (recipe_kind) { includes(:recipe_kind).where(recipe_kinds: {name: recipe_kind}) }
 
-  def self.random(recipe_kind)
-    if (recipe_kind == "false")
-      Recipe.all[rand(Recipe.all.length)]
-    else
-      recipes = RecipeKind.find_by(name: recipe_kind).recipes
-      recipes[rand(recipes.length)]
-    end
-  end
+  belongs_to :recipe_kind
 
   def show
     name + "\n( " + url + " ) "
