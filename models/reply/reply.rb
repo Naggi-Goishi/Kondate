64647bfdf56c2d8d88293e9a8f308101b8034868
Naggi-Goishi <naggitapdance@gmail.com>
2017-04-29 20:57:40 -0700

    debug

--- models/reply/reply.rb
+++ models/reply/reply.rb
@@ -24,7 +24,7 @@ class Reply
         @client.reply_message(@event['replyToken'], content)
       end
     else
-      @content.reply_message(@event['replyToken'], @content)
+      @client.reply_message(@event['replyToken'], @content)
     end
   end
 
