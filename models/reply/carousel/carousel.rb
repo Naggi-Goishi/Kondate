cf572932957982b435e92c1699dba2f25a118b64
Naggi-Goishi <naggitapdance@gmail.com>
2017-04-29 21:21:15 -0700

    debug

--- models/reply/carousel/carousel.rb
+++ models/reply/carousel/carousel.rb
@@ -6,13 +6,66 @@ class Carousel
   end
 
   def build
-    {
-      "type": "template",
-      "altText": "this is a carousel template",
-      "template": {
-          "type": "carousel",
-          "columns": @columns.map { |column| column.build }
-      }
-    }
+{
+  "type": "template",
+  "altText": "this is a carousel template",
+  "template": {
+      "type": "carousel",
+      "columns": [
+          {
+            "thumbnailImageUrl": "https://example.com/bot/images/item1.jpg",
+            "title": "this is menu",
+            "text": "description",
+            "actions": [
+                {
+                    "type": "postback",
+                    "label": "Buy",
+                    "data": "action=buy&itemid=111"
+                },
+                {
+                    "type": "postback",
+                    "label": "Add to cart",
+                    "data": "action=add&itemid=111"
+                },
+                {
+                    "type": "uri",
+                    "label": "View detail",
+                    "uri": "http://example.com/page/111"
+                }
+            ]
+          },
+          {
+            "thumbnailImageUrl": "https://example.com/bot/images/item2.jpg",
+            "title": "this is menu",
+            "text": "description",
+            "actions": [
+                {
+                    "type": "postback",
+                    "label": "Buy",
+                    "data": "action=buy&itemid=222"
+                },
+                {
+                    "type": "postback",
+                    "label": "Add to cart",
+                    "data": "action=add&itemid=222"
+                },
+                {
+                    "type": "uri",
+                    "label": "View detail",
+                    "uri": "http://example.com/page/222"
+                }
+            ]
+          }
+      ]
+  }
+}
+    # {
+    #   "type": "template",
+    #   "altText": "this is a carousel template",
+    #   "template": {
+    #       "type": "carousel",
+    #       "columns": @columns.map { |column| column.build }
+    #   }
+    # }
   end
 end
