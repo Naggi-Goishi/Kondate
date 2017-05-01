57834b8bafbf967f396ea43fab0490180153340d
Naggi-Goishi <naggitapdance@gmail.com>
2017-05-01 13:17:16 -0700

    debug

--- collections/ingredients.rb
+++ collections/ingredients.rb
@@ -18,7 +18,7 @@ class Ingredients < ActiveRecord::Relation
 
   def show
     return false if blank?
-    @records.inject("レシピ\n") do |text, ingredient|
+    @records.inject('') do |text, ingredient|
       return text + '...' if text.length > 55
       text + ingredient.name + '、'
     end[0..-2]
