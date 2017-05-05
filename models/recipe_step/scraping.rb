0cd11fe12c74c4d13c17ce05bd10ff2197ad8de3
Naggi-Goishi <naggitapdance@gmail.com>
2017-05-05 14:29:10 -0700

    debug

--- models/recipe_step/scraping.rb
+++ models/recipe_step/scraping.rb
@@ -10,12 +10,13 @@ class RecipeStep < ActiveRecord::Base
         Recipe.all.each do |recipe|
           page = agent.get(recipe.url)
           steps = page.search('.list_flow01 li')
-          step = recipe.steps.build
           steps.each do |step_ele|
+            step = recipe.steps.build
             step.step_num = step_ele.search('.num').inner_text.to_i
             step.thumbnail_image_url = step_ele.search('img').first[:src] unless step_ele.search('img').blank?
             step.text = step_ele.search('.txt').inner_text
             step.save
+            print '#'
           end
         end
       end
