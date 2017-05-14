3c57a18464be60f626cd586a833ff450aef9df07
Naggi Goishi <naggitapdance@gmail.com>
2017-05-13 17:21:26 -0700

    update

--- models/recipe/recipe.rb
+++ models/recipe/recipe.rb
@@ -9,13 +9,22 @@ class Recipe < ActiveRecord::Base
   scope :has_ingredient_hiragana, -> (hiragana) { joins(:ingredients).where(ingredients: { hiragana: hiragana }) }
   scope :has_ingredient, ->  (ingredient)  { has_ingredient_hiragana(ingredient.try(:hiragana)) }
   scope :has_ingredients, -> (ingredients) {
-    joins(:ingredients).where(ingredients: { hiragana: ingredients.pluck(:hiragana) } ) if ingredients 
+    has_ingredient(ingredients.uncommon[0]).select(&[:has_all_ingredients?, ingredients[1..-1]])
   }
+  scope :has_recipe_kinds_name, -> (recipe_kinds) { joins(:recipe_kind).where(recipe_kinds: { name: recipe_kinds }) }
 
   belongs_to :recipe_kind
   has_many :ingredients_recipes
   has_many :ingredients, through: :ingredients_recipes
   has_many :steps, class_name: 'RecipeStep'
+
+  def has_ingredient?(ingredient)
+    ingredients.include?(ingredient)
+  end
+
+  def has_all_ingredients?(ingredients)
+    ingredients.all? { |ingredient| has_ingredient?(ingredient) }
+  end
 
 private
   def counts_recipe
